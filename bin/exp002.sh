6adc0a9cf840c8cebdc3cb2d8a94cd49a3dbc23a
huutrinh <huutrinh@huutrinhs-MacBook-Air.local>
2019-11-26 13:25:22 +0900

    build pipeline

--- bin/exp002.sh
+++ bin/exp002.sh
@@ -4,6 +4,6 @@ exp_id=002
 model=config${exp_id}
 config=config/${model}.py
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3 
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
 
 python -m exps.exp${exp_id} ${config} --fold 0
