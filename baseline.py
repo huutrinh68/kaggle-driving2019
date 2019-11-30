b62a1cb7d9f2bdbc6ef403b9c62f712ca2443838
trinhnh1 <huutrinh@huutrinhs-MacBook-Pro.local>
2019-11-30 17:35:52 +0900

    fix bug in loss

--- baseline.py
+++ baseline.py
@@ -1,7 +1,8 @@
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import cv2
-from tqdm import tqdm_notebook as tqdm
+# from tqdm import tqdm_notebook as tqdm
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 import seaborn as sns
 from functools import reduce
