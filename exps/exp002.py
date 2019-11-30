6632cde9f77bb0b02db973d9708117bfbbb7a0cf
trinhnh1 <huutrinh@huutrinhs-MacBook-Pro.local>
2019-11-30 19:42:16 +0900

    fix bug in loss

--- exps/exp002.py
+++ exps/exp002.py
@@ -204,7 +204,7 @@ def run_nn(
                 loss.backward() # accumulate loss
             
             if (i+1) % cfg.n_grad_acc == 0 or (i+1) == len(loader):
-                torch.nn.utils.clip_grad_norm(model.parameters(), 1.0)
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
                 optimizer.step() # update
                 optimizer.zero_grad() # flush
 
