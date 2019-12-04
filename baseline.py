e188dd2e75eb17dd4f25080aa15d1a420f2cb09e
huutrinh <huutrinh@huutrinhs-MacBook-Air.local>
2019-12-04 10:53:43 +0900

    add baseline

--- baseline.py
+++ baseline.py
@@ -670,3 +670,78 @@ plt.plot(series3.index, series3,label = 'dev loss');
 plt.savefig('eda/history.png')
 
 
+DISTANCE_THRESH_CLEAR = 2
+
+def convert_3d_to_2d(x, y, z, fx = 2304.5479, fy = 2305.8757, cx = 1686.2379, cy = 1354.9849):
+    # stolen from https://www.kaggle.com/theshockwaverider/eda-visualization-baseline
+    return x * fx / z + cx, y * fy / z + cy
+
+def optimize_xy(r, c, x0, y0, z0):
+    def distance_fn(xyz):
+        x, y, z = xyz
+        x, y = convert_3d_to_2d(x, y, z0)
+        y, x = x, y
+        x = (x - IMG_SHAPE[0] // 2) * IMG_HEIGHT / (IMG_SHAPE[0] // 2) / MODEL_SCALE
+        x = np.round(x).astype('int')
+        y = (y + IMG_SHAPE[1] // 4) * IMG_WIDTH / (IMG_SHAPE[1] * 1.5) / MODEL_SCALE
+        y = np.round(y).astype('int')
+        return (x-r)**2 + (y-c)**2
+    
+    res = minimize(distance_fn, [x0, y0, z0], method='Powell')
+    x_new, y_new, z_new = res.x
+    return x_new, y_new, z0
+
+def clear_duplicates(coords):
+    for c1 in coords:
+        xyz1 = np.array([c1['x'], c1['y'], c1['z']])
+        for c2 in coords:
+            xyz2 = np.array([c2['x'], c2['y'], c2['z']])
+            distance = np.sqrt(((xyz1 - xyz2)**2).sum())
+            if distance < DISTANCE_THRESH_CLEAR:
+                if c1['confidence'] < c2['confidence']:
+                    c1['confidence'] = -1
+    return [c for c in coords if c['confidence'] > 0]
+
+
+def extract_coords(prediction):
+    logits = prediction[0]
+    regr_output = prediction[1:]
+    points = np.argwhere(logits > 0)
+    col_names = sorted(['x', 'y', 'z', 'yaw', 'pitch_sin', 'pitch_cos', 'roll'])
+    coords = []
+    for r, c in points:
+        regr_dict = dict(zip(col_names, regr_output[:, r, c]))
+        coords.append(_regr_back(regr_dict))
+        coords[-1]['confidence'] = 1 / (1 + np.exp(-logits[r, c]))
+        coords[-1]['x'], coords[-1]['y'], coords[-1]['z'] = optimize_xy(r, c, coords[-1]['x'], coords[-1]['y'], coords[-1]['z'])
+    coords = clear_duplicates(coords)
+    return coords
+
+def coords2str(coords, names=['yaw', 'pitch', 'roll', 'x', 'y', 'z', 'confidence']):
+    s = []
+    for c in coords:
+        for n in names:
+            s.append(str(c.get(n, 0)))
+    return ' '.join(s)
+
+
+predictions = []
+
+test_loader = DataLoader(dataset=test_dataset, batch_size=4, shuffle=False, num_workers=4)
+
+model.eval()
+
+for img, _, _ in tqdm(test_loader):
+    with torch.no_grad():
+        output = model(img.to(device))
+    output = output.data.cpu().numpy()
+    for out in output:
+        coords = extract_coords(out)
+        s = coords2str(coords)
+        predictions.append(s)
+
+
+test = pd.read_csv(PATH + 'sample_submission.csv')
+test['PredictionString'] = predictions
+test.to_csv('predictions.csv', index=False)
+test.head()
